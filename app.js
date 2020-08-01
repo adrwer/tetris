ae396810ff6c50d8e3779f94544b677200eea9f3
Adrian Werimo <adrwer7@gmail.com>
2020-08-01 14:16:40 +0300

    JS variable and constant assignment

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 // Execute after the html content (excluding images and css) is loaded
 document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+  const width = 10
 
+  console.log(squares)
 })
