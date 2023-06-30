65515daa4fc5ca03a083ac5ce5ae516c56507846
Adrian Werimo <adrwer7@gmail.com>
2023-06-30 10:47:34 +0300

    Modified score element

--- app.js
+++ app.js
@@ -211,7 +211,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // Game over
   function gameOver() {
     if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      scoreDisplay.innerHTML = 'end'
       clearInterval(timerId)
     }
   }
